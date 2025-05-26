e6d371d90167741dc3f54ad80af0565203ed19bf
Aleksander_hub <oshepkov77@gmail.com>
2025-05-26 10:28:33 +0300

    ok8

--- project/main/views.py
+++ project/main/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'index': 'Главная страница'
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
-    return render(request, 'main/about.html')
+    data = {
+        'about': 'Про нас'
+    }
+    return render(request, 'main/about.html', data)
     
 def contacts(request):
-    return render(request, 'main/contacts.html')
+    data = {
+        'contacts': 'Контакты'
+    }
+    return render(request, 'main/contacts.html', data)
 
