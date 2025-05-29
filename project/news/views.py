dfab7d4bcd899e02145e5c045d33399dd9ab5ebe
Aleksander_hub <oshepkov77@gmail.com>
2025-05-29 09:47:30 +0300

    ok11

--- project/news/views.py
+++ project/news/views.py
@@ -4,3 +4,6 @@ from .models import Articles
 def news(request):
     all_articles = Articles.objects.order_by('-date')
     return render(request,'news/news.html', {'news': all_articles})
+
+def create(request):
+    return render(request,'news/create.html')
